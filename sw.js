8cbcc0448fa41f6da6563777ff26d77f8c7dd5d7
BossGangVIP <2upinvesting@gmail.com>
2025-09-06 23:14:45 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', () => {}); // network-only; no caching to avoid stale assets during rapid changes
